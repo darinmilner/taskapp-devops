caf4166075c4f03423069da0548ff94fde959898
darinmilner <darin.milner@gmail.com>
2023-06-07 19:32:58 +0700

    s3 code updated

--- Jenkins/Pipeline/buildAPI.groovy
+++ Jenkins/Pipeline/buildAPI.groovy
@@ -11,6 +11,7 @@ def runAPITests() {
 
     try {
         sh "gradle clean test --info"
+        sh "pwd"
         sh "cat build/reports/tests/index.html"
     } catch (Exception err) {
         echo "Error running test $err"
