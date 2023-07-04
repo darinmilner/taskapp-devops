8136a0c7c80e537a4eab09cae78a45bf43d2c5f1
kallada2021 <karthikeyaallada77@gmail.com>
2023-07-04 11:45:42 -0500

    Added session instead of config at line#43.

--- Jenkins/Scripts/get_file.py
+++ Jenkins/Scripts/get_file.py
@@ -1,9 +1,8 @@
 import boto3
-import json
 import logging
 import sys
-from botocore.config import Config
 from botocore.exceptions import ClientError
+from setup import Setup
 
 USEAST1_BUCKET_NAME = "taskapi-storage-useast1"
 def get_latest_envfile(access_key, secret_key):
@@ -14,18 +13,14 @@ def get_latest_envfile(access_key, secret_key):
     )
     s3_resource = session.resource('s3', region_name='us-east-1')
     latest_file = None
-    # TODO: find bug and download file from useast1 bucket by calling download_file function below
     try:
         # TODO: add BucketName to Setup class
         files = list(s3_resource.Bucket(USEAST1_BUCKET_NAME).objects.filter(Prefix="envfiles/"))
         files.sort(key=lambda x: x.last_modified)
         latest_file = files[-1].key
-        print(latest_file)
         download_file(access_key, secret_key, latest_file)
-        #return latest_file
     except ClientError as e:
         logging.error(e)
-    print(latest_file)
     return latest_file
 
 def download_file(access_key, secret_key, file_name):
@@ -34,36 +29,23 @@ def download_file(access_key, secret_key, file_name):
         aws_secret_access_key=secret_key,
         region_name='us-east-1'
     )
-    #s3_resource = session.resource('s3', region_name='us-east-1')
-    print(file_name)
-    print(USEAST1_BUCKET_NAME)
     source_s3 = session.client('s3')
     source_s3.download_file(USEAST1_BUCKET_NAME, file_name, file_name)
 
-    # TODO use boto3 s3 Object and download_file functions to download the latest file
 
-
-def upload_envfile_to_regional_bucket(access_key, secret_key, file, bucket, region, object=None):
+def upload_envfile_to_regional_bucket(setup, file, object=None):
     session = boto3.Session(
-        aws_access_key_id=access_key,
-        aws_secret_access_key=secret_key,
-        region_name=region
+        aws_access_key_id=setup.access_key,
+        aws_secret_access_key=setup.secret_key,
+        region_name=setup.region
     )
     s3_client = session.client("s3")
-    # config = Config(
-    #     aws_access_key_id=access_key,
-    #     aws_secret_access_key=secret_key,
-    #     region_name=region
-    # )
-    print(file)
-    print(bucket)
     if object is None:
         object = f"envfiles/{file}"
     try:
-        s3_client.upload_file(file, bucket, file)
+        s3_client.upload_file(file, setup.bucket, file)
     except ClientError as e:
         logging.error(e)
- # TODO: use boto3 client to upload the env file to the bucket using upload_file method
 
 
 access_key = sys.argv[1]
@@ -71,5 +53,8 @@ secret_key = sys.argv[2]
 upload_region = sys.argv[3]
 upload_bucket = sys.argv[4]
 
+s3Setup = Setup(upload_region, upload_bucket, access_key, secret_key)
 latestfile = get_latest_envfile(access_key, secret_key)
-upload_envfile_to_regional_bucket(access_key, secret_key, latestfile, upload_bucket, upload_region)
+#upload_envfile_to_regional_bucket(access_key, secret_key, latestfile, upload_bucket, upload_region)
+upload_envfile_to_regional_bucket(s3Setup, latestfile)
+
--- Jenkins/Scripts/setup.py
+++ Jenkins/Scripts/setup.py
@@ -4,6 +4,11 @@ class Setup():
         TODO: finish the configuration variables
     """
 
-    def __init__(self, region):
+    def __init__(self, region, bucket, access_key, secret_key):
         self.envfile_region = "us-east-1"
         self.region = region
+        self.bucket = bucket
+        self.access_key = access_key
+        self.secret_key = secret_key
+
+
