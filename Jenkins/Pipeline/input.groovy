a2aa8228639f2bad46f6e56680e3bc674c621668
darinmilner <darin.milner@gmail.com>
2023-07-06 20:41:48 +0700

    Jenkins code refactored

--- Jenkins/Pipeline/input.groovy
+++ Jenkins/Pipeline/input.groovy
@@ -1,6 +1,11 @@
 boolean getInput(String message, String description) {
     String inputChoice
-    choice(name: "REGIONS", choices: ["All Regions", "Selected Regions"], description: description)
+    input(
+            parameters: [
+                    choice(name: "REGIONS", choices: ["All Regions", "Selected Regions"], description: description)
+            ]
+    )
+
     inputChoice = params.REGIONS
     echo "$inputChoice chosen"
     return inputChoice
