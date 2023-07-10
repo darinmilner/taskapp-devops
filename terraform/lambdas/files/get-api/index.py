c0a5d22fe3b0cd888650f249967222331c10b510
darinmilner <darin.milner@gmail.com>
2023-07-10 19:18:50 +0700

    Lambda code refactored

--- terraform/lambdas/files/get-api/index.py
+++ terraform/lambdas/files/get-api/index.py
@@ -22,18 +22,18 @@ def lambda_handler(event, context):
     latest_file_folder = ""  # TODO: get values from json
 
     s3setup = Setup(upload_region, upload_bucket, access_key, secret_key, latest_file_folder)
-    s3latestfile = get_latest_envfile(s3setup)
+    s3latestfile = get_latest_apifile(s3setup)
 
     if upload_region != s3setup.envfile_region:
-        upload_envfile_to_regional_bucket(s3setup, s3latestfile)
+        upload_apifile_to_regional_bucket(s3setup, s3latestfile)
 
 
-def get_latest_envfile(s3setup):
+def get_latest_apifile(s3setup):
     session = boto3.Session(
         aws_access_key_id=s3setup.access_key,
         aws_secret_access_key=s3setup.secret_key,
     )
-    s3_resource = session.resource('s3', region_name=s3setup.envfile_region)
+    s3_resource = session.resource('s3', region_name=s3setup.apifile_region)
     latest_file = None
     try:
         files = list(s3_resource.Bucket(USEAST1_BUCKET).objects.filter(Prefix=s3setup.folder))
@@ -52,25 +52,25 @@ def download_file(s3setup, file_name):
         region_name=s3setup.envfile_region
     )
     source_s3 = session.client('s3')
-    downloaded_file = "application-prod.yaml"
+    downloaded_file = "latest-api.zip"
     logger.info(downloaded_file)
     # file_name is file to download  downloaded file is name after downloading
     source_s3.download_file(USEAST1_BUCKET, file_name, downloaded_file)
 
 
-def upload_envfile_to_regional_bucket(setup, file):  # object=None):
+def upload_apifile_to_regional_bucket(setup, file):  # object=None):
     session = boto3.Session(
         aws_access_key_id=setup.access_key,
         aws_secret_access_key=setup.secret_key,
         region_name=setup.region
     )
 
-    latest_envfile = f"api/latest-api.zip"
-    logger.info(f"Upload file object path {latest_envfile}\n")
+    latest_apifile = f"api/latest-api.zip"
+    logger.info(f"Upload file object path {latest_apifile}\n")
     s3_client = session.client("s3")
 
     file = "application-prod.yaml"
     try:
-        s3_client.upload_file(file, setup.bucket, latest_envfile)
+        s3_client.upload_file(file, setup.bucket, latest_apifile)
     except ClientError as e:
         logging.error(e)
--- terraform/lambdas/files/get-api/setup.py
+++ terraform/lambdas/files/get-api/setup.py
@@ -1,5 +1,5 @@
 def __init__(self, region, bucket, access_key, secret_key, latest_folder):
-    self.envfile_region = "us-east-1"
+    self.apifile_region = "us-east-1"
     self.folder = latest_folder
     self.region = region
     self.bucket = bucket
